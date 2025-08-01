cd3e1fe8db89224e15dfe677cdc834b233dbfa26
Alan Pisani <alanpisani1993@gmail.com>
2025-07-31 23:28:48 -0300

    Hero terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const navItems = document.querySelectorAll(".nav-list li a");
 
+const downloadBtn = document.getElementById('download-cv-btn');
+const seeBtn = document.getElementById('view-cv-btn');
+
 navItems.forEach(item =>{
     item.addEventListener("click", () => {
         navItems.forEach(item => {
@@ -8,4 +11,23 @@ navItems.forEach(item =>{
         });
         item.classList.add("nav-link-focus");
     });
+});
+
+const typed = new Typed('#typed', {
+  strings: ["Desarrollador Backend", "Creativo", "Sólido"], 
+  typeSpeed: 80,
+  backSpeed: 40,
+  loop: true
+});
+
+
+downloadBtn.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = '../download/cv_alan_pisani.pdf';
+    link.download = 'cv_alan_pisani.pdf';
+    link.click();
+});
+
+seeBtn.addEventListener('click', () => {
+    window.open('../download/cv_alan_pisani.pdf', '_blank');
 });
