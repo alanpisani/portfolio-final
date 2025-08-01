83934153bb865ba8af09e56e56a605bb09950838
Alan Pisani <alanpisani1993@gmail.com>
2025-08-01 09:40:20 -0300

    responsive 768px casi terminado

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ import "./typed.js";
 import "./hero.js";
 import "./my-proyects.js";
 import "./contact.js";
+
+Swal.fire("En algunas pantallas puede que no se vea del todo bien. Dame tiempo para solucionarlo");
