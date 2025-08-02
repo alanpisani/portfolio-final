f64ad8ea2e7b02c7564455a1008dd2e2b12ce6bf
Alan Pisani <alanpisani1993@gmail.com>
2025-08-02 16:17:19 -0300

    responsive tablet implementado

--- js/script.js
+++ js/script.js
@@ -3,10 +3,3 @@ import "./typed.js";
 import "./hero.js";
 import "./my-proyects.js";
 import "./contact.js";
-
-Swal.fire({
-    title:"Web aún en construcción 👷‍♂️",
-    text:"Puede que no se vea del todo bien. Dame tiempo para solucionarlo.",
-    confirmButtonText: 'Aceptar',
-    confirmButtonColor: 'var(--secundary-color)'
-});
